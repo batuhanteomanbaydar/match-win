7393474f8191334102de17eb3cd12dc57049a623
batuhanteomanbaydar <klixcolombia@gmail.com>
2019-11-16 15:57:15 +0300

    register fragment added

--- app/src/main/java/com/example/matchwin/MainActivity.kt
+++ app/src/main/java/com/example/matchwin/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.matchwin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadRegister(Register())
+    }
+
+    private fun loadRegister(frag1: Register) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.root_layout, frag1)
+        ft.commit()
     }
 }
---
+++ app/src/main/java/com/example/matchwin/Register.kt
@@ -0,0 +1,21 @@
+package com.example.matchwin
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Register : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return LayoutInflater.from(container?.context).inflate(R.layout.fragment_register, container, false)
+    }
+
+}
